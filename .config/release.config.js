cabac89546177e70ed5f9510b10ad64a3177a29a
Daniel Mallett <daniel.mallett@ninjamonkeygames.com>
2026-08-16 16:23:22 +0100

    feat(core): ci test
    Files:
    
    - N/A
    
    Description:
    
    CI test.
    
    References #1
    
    Signed-off-by: Daniel Mallett <daniel.mallett@ninjamonkeygames.com>

--- .config/release.config.js
+++ .config/release.config.js
@@ -1,20 +1,31 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
 export default {
-  branches: [
-    'master',
-    { name: 'release/*', prerelease: 'rc', channel: 'staging' },
-    { name: 'feature/*', prerelease: 'dev' }
-  ],
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
     [
       '@semantic-release/npm',
       {
-        npmPublish: true
-      }
+        npmPublish: true,
+        pkgRoot: '.',
+      },
     ],
-    '@semantic-release/git',
-    '@semantic-release/github'
-  ]
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
 };
